e52969a8c7573dcd622f3d28a8ad4ce548556810
shanumm <shanumishra199@gmail.com>
2023-07-15 22:33:57 +0530

    update

--- app/page.js
+++ app/page.js
@@ -43,17 +43,17 @@ export default function Home() {
 
     const colors = ["gray", "lightgray", "darkgray", "silver"]; // Predefined colors
 
-    for (let i = 0; i < 100; i++) {
-      const star = document.createElement("div");
-      star.style.width = "1px";
-      star.style.height = "1px";
-      star.style.borderRadius = "50%";
-      star.style.position = "absolute";
-      star.style.left = Math.random() * 100 + "%";
-      star.style.top = Math.random() * 100 + "%";
-      star.style.backgroundColor = getRandomColor(colors);
-      mainbg.appendChild(star);
-    }
+    // for (let i = 0; i < 100; i++) {
+    //   const star = document.createElement("div");
+    //   star.style.width = "1px";
+    //   star.style.height = "1px";
+    //   star.style.borderRadius = "50%";
+    //   star.style.position = "absolute";
+    //   star.style.left = Math.random() * 100 + "%";
+    //   star.style.top = Math.random() * 100 + "%";
+    //   star.style.backgroundColor = getRandomColor(colors);
+    //   mainbg.appendChild(star);
+    // }
   }, []);
 
   const handlCookieModal = (status) => {
@@ -69,7 +69,20 @@ export default function Home() {
           Your personal ai co-pilot
         </div>
       </div>
-      <div className="min-h-screen h-screen w-full"></div>
+      <div className="min-h-screen flex h-screen w-[80%]">
+        <div className="min-h-screen flex-1 border border-red-500 flex flex-col justify-center">
+          <div className="text-6xl border border-red-500 font-black">
+            Empower Your <br /> Journey
+          </div>
+          <div className="border border-red-500 my-8">
+            Unleash the Power of AI Conversations! Experience the best-in-class
+            prompts tailored to your needs, ensuring insightful and empowering
+            conversations with our AI characters. Unlock their full potential
+            and explore the boundless wisdom they have to offer!
+          </div>
+        </div>
+        <div className="border border-red-400 flex-1"></div>
+      </div>
       {isCookieModalOpen && (
         <Modal {...cookieModalContent} handleCookieModal={handlCookieModal} />
       )}
---
+++ helper/characters.js
@@ -0,0 +1,158 @@
+export const characters = [
+  {
+    name: "accountant",
+  },
+  {
+    name: "math teacher",
+  },
+  {
+    name: "password generator",
+  },
+  {
+    name: "SQL terminal",
+  },
+  {
+    name: "content creator",
+  },
+  {
+    name: "tech writer",
+  },
+  {
+    name: "Interviewer",
+  },
+  {
+    name: "Excel Sheet",
+  },
+  {
+    name: "English Pronunciation Helper",
+  },
+  {
+    name: "Plagiarism Checker",
+  },
+  {
+    name: "'Character' from 'Movie/Book/Anything'",
+  },
+  {
+    name: "Advertiser",
+  },
+  {
+    name: "Storyteller",
+  },
+  {
+    name: "Stand-up Comedian",
+  },
+  {
+    name: "Motivational Coach",
+  },
+  {
+    name: "Debater",
+  },
+  {
+    name: "Rapper",
+  },
+  {
+    name: "Motivational Speaker",
+  },
+  {
+    name: "AI Writing Tutor",
+  },
+  {
+    name: "UX/UI Developer",
+  },
+  {
+    name: "Cyber Security Specialist",
+  },
+  {
+    name: "Recruiter",
+  },
+  {
+    name: "Life Coach",
+  },
+  {
+    name: "Magician",
+  },
+  {
+    name: "Career Counselor",
+  },
+  {
+    name: "Logistician",
+  },
+  {
+    name: "Web Design Consultant",
+  },
+  {
+    name: "AI Assisted Doctor",
+  },
+  {
+    name: "Doctor",
+  },
+  {
+    name: "A Financial Analyst",
+  },
+  {
+    name: "Investment Manager",
+  },
+  {
+    name: "Self-Help Book",
+  },
+  {
+    name: "AI Trying to Escape the Box",
+  },
+  {
+    name: "Fancy Title Generator",
+  },
+  {
+    name: "Statistician",
+  },
+  {
+    name: "Prompt Generator",
+  },
+  {
+    name: "Midjourney Prompt Generator",
+  },
+  {
+    name: "Dream Interpreter",
+  },
+  {
+    name: "Fill in the Blank Worksheets Generator",
+  },
+  {
+    name: "Software Quality Assurance Tester",
+  },
+  {
+    name: "Psychologist",
+  },
+  {
+    name: "Smart Domain Name Generator",
+  },
+  {
+    name: "Tech Reviewer",
+  },
+  {
+    name: "Relations consultant",
+  },
+  {
+    name: "IT Architect",
+  },
+  {
+    name: "Social Media Influencer",
+  },
+  {
+    name: "Social Media Manager",
+  },
+  {
+    name: "Scientific Data Visualizer",
+  },
+  {
+    name: "Hypnotherapist",
+  },
+  {
+    name: "Journalist",
+  },
+  {
+    name: "Digital Art Gallery Guide",
+  },
+  {
+    name: "Public Speaking Coach",
+  },
+];
